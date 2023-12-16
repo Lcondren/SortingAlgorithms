139628a17b3dacb232679be5cb7fbc132e0bef9a
Luke Condren <151095025+Lcondren@users.noreply.github.com>
2023-12-16 14:39:49 +0000

    Benchmarking.java

---
+++ Benchmarking.java
@@ -0,0 +1,106 @@
+package ie.atu.sw;
+
+public class Benchmarking {
+
+    private String sort;
+    private String _100;
+    private String _250;
+    private String _500;
+    private String _1000;
+    private String _1750;
+    private String _2500;
+    private String _5000;
+
+
+    private String _7500;
+
+    private String _8500;
+    private String _10000;
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String get_100() {
+        return _100;
+    }
+
+    public void set_100(String _100) {
+        this._100 = _100;
+    }
+
+    public String get_250() {
+        return _250;
+    }
+
+    public void set_250(String _250) {
+        this._250 = _250;
+    }
+
+    public String get_500() {
+        return _500;
+    }
+
+    public void set_500(String _500) {
+        this._500 = _500;
+    }
+
+    public String get_1000() {
+        return _1000;
+    }
+
+    public void set_1000(String _1000) {
+        this._1000 = _1000;
+    }
+
+    public String get_1750() {
+        return _1750;
+    }
+
+    public void set_1750(String _1750) {
+        this._1750 = _1750;
+    }
+
+    public String get_2500() {
+        return _2500;
+    }
+
+    public void set_2500(String _2500) {
+        this._2500 = _2500;
+    }
+
+    public String get_5000() {
+        return _5000;
+    }
+
+    public void set_5000(String _5000) {
+        this._5000 = _5000;
+    }
+
+    public String get_7500() {
+        return _7500;
+    }
+
+    public String get_8500() {
+        return _8500;
+    }
+
+    public void set_8500(String _8500) {
+        this._8500 = _8500;
+    }
+    public void set_7500(String _7500) {
+        this._7500 = _7500;
+    }
+
+    public String get_10000() {
+        return _10000;
+    }
+
+    public void set_10000(String _10000) {
+        this._10000 = _10000;
+    }
+}
